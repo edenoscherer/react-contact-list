f3622a026dbe6472804da4ed7003bc26a7625fcd
Edeno Scherer <edenoscherer@gmail.com>
2022-07-08 23:11:45 -0300

    feat: doc star project

---
+++ README.md
@@ -0,0 +1,28 @@
+# Projeto teste lista de contatos
+
+Projeto desenvolvido para testar a combinação do React, Typescipt, Material UI, Formik e Yup
+
+
+Antes de testar, não esqueça de renomear o arquivo .env.development.example para .env.development e atualizar o endereço da api.
+
+## Instruções para executar o projeto
+
+**Antes de executar o projeto deve ser feito a instalação das dependências**
+
+```bash
+# NPM
+npm install
+
+# YARN
+yarn install
+```
+
+**Executar o projeto em modo de desenvolvimento**
+
+```bash
+# NPM
+npm run dev
+
+# YARN
+yarn dev
+```
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
-import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 import React from "react";
 
 import PageContent from "../../components/page-content";
@@ -7,32 +8,23 @@ import PageContent from "../../components/page-content";
 const Home: React.FC = () => {
   return (
     <PageContent>
-      <Stack
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
-      >
-        <Button variant="contained">empty</Button>
-        <Button variant="contained" color="primary">
-          primary
-        </Button>
-        <Button variant="contained" color="secondary">
-          secondary
-        </Button>
-        <Button variant="contained" color="success">
-          success
-        </Button>
-        <Button variant="contained" color="info">
-          info
-        </Button>
-        <Button variant="contained" color="warning">
-          warning
-        </Button>
-        <Button variant="contained" color="error">
-          error
-        </Button>
-      </Stack>
+      <Paper sx={{ p: 2 }}>
+        <Stack direction="column" spacing={2}>
+          <Typography variant="h4" textAlign="center">
+            Projeto teste lista de contatos
+          </Typography>
+          <Typography variant="body1" textAlign="center">
+            Projeto desenvolvido para testar a combinação do React, Typescipt,
+            Material UI, Formik e Yup
+          </Typography>
+
+          <Typography variant="body1" textAlign="center">
+            Antes de testar, não esqueça de renomear o arquivo
+            .env.development.example para .env.development e atualizar o
+            endereço da api.
+          </Typography>
+        </Stack>
+      </Paper>
     </PageContent>
   );
 };
